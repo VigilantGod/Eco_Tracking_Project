e83b8dbeeecc8d6596f1c0d5af4bf04098669b67
Easara <easarakulindu@gmail.com>
2026-01-10 18:05:45 +0530

    add map module

---
+++ modules/map.py
@@ -0,0 +1,25 @@
+import folium
+from geopy import Nominatim
+import routingpy
+from streamlit_js_eval import get_geolocation
+import geocoder
+
+
+def getLocation():
+    """returns tuple of location cords and Accuracy"""
+    location = get_geolocation()
+    #if coudn't get location this will return None
+    if location:
+        longitude = location["coords"]["longitude"]
+        latitude = location["coords"]["latitude"]
+        userLocation = [latitude,longitude]
+
+        accuracy = location["coords"]["accuracy"]
+        
+        return  (userLocation,accuracy)
+
+def get_map(location,zoom_start:int,width:int,height:int):
+    return folium.Map
+def getLocationAproximately():
+    g_ip = geocoder.ip("me")
+    return g_ip.latlng
