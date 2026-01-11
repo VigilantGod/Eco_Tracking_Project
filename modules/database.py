65c9029526bc1a395dd8730aa2473f5718aec7f6
Easara <easarakulindu@gmail.com>
2026-01-11 10:43:31 +0530

    fixed a path error

--- modules/database.py
+++ modules/database.py
@@ -1,10 +1,10 @@
-from sqlalchemy import create_engine,Column,String,Integer
-from sqlalchemy.orm import declarative_base,sessionmaker
+from sqlalchemy import create_engine,Column,String,Float,ForeignKey
+from sqlalchemy.orm import declarative_base,sessionmaker,Mapped,mapped_column
 import os
 
 #creates the data directory if not exist
 if not os.path.exists("data"):
-    os.mkdir()
+    os.mkdir("data")
 
 #database url
 db_URL = "sqlite:///data/ecotrack.db"
@@ -18,10 +18,25 @@ class Users(base):
     __tablename__ = "users"
 
     username = Column(String,primary_key=True,index=True)
+    full_name = Column(String,nullable=False)
+    phone_number = Column(String,nullable=True)
     password = Column(String(128),nullable=False)
     email = Column(String,nullable=False)
     phone_number = Column(String,nullable=True)
 
+class Parcel_Details(base):
+    __tablename__ = "parcel_details"
+
+    username = Column(String,ForeignKey("users.username"))
+    parcelId = Column(String,primary_key=True,nullable=False,index=True)
+    parcel_type = Column(String,index=True)
+    start_loc_lat = Column(Float,nullable=False)
+    start_loc_lon = Column(Float,nullable=False)
+    end_loc_lat = Column(Float,nullable=False)
+    end_loc_lon = Column(Float,nullable=False)
+    is_fragile = Column(Float,nullable=False)
+    is_gift = Column(Float,nullable=False)
+    description = Column(String,nullable=True)
 
 base.metadata.create_all(bind=engine)
 
@@ -31,9 +46,9 @@ def get_db():
     """
     return session()
 
-def  store_user(db,user:str,email:str,phone_number:str,hashed_password:str):
+def  store_user(db,full_name:str,user:str,email:str,phone_number:str,hashed_password:str):
     """store a user details in the database"""
-    new_user = Users(username=user,email=email,phone_number=phone_number,password=hashed_password)
+    new_user = Users(full_name=full_name,username=user,email=email,phone_number=phone_number,password=hashed_password)
     #add new_user to the temporary memory of db
     db.add(new_user)
     #permenently saving details
