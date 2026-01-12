9f6119996d3448d13c8dafb5123009de95c8702f
Easara <easarakulindu@gmail.com>
2026-01-12 07:09:14 +0530

    Add logout button

--- app.py
+++ app.py
@@ -11,19 +11,23 @@ def main():
     if 'logged_in' not in st.session_state:
         st.session_state.logged_in = False
 
-    loginPage = st.Page(page="web_pages/loginPage.py",icon=":material/login:")
-    registerPage = st.Page(page="web_pages/registerPage.py",icon=":material/person_add:")
-    trackingPage = st.Page("web_pages/trackingPage.py",icon=":material/location_on:")
-    routingPage = st.Page("web_pages/userOrderPage.py",icon=":material/route:")
-    ticketingPage = st.Page("web_pages/FeedbackPage.py",icon=":material/confirmation_number:")
+    loginPage = st.Page(page="web_pages/loginPage.py",icon=":material/login:",title="Login")
+    registerPage = st.Page(page="web_pages/registerPage.py",icon=":material/person_add:",title="Register")
+    trackingPage = st.Page("web_pages/trackingPage.py",icon=":material/location_on:",title="Tracking")
+    routingPage = st.Page("web_pages/userOrderPage.py",icon=":material/route:",title="Place Order")
+    feedbackPage = st.Page("web_pages/FeedbackPage.py",icon=":material/confirmation_number:", title="Feedback")
     
     if not st.session_state.logged_in:
         pg = st.navigation([loginPage, registerPage])
         pg.run()
     else:
-        pg = st.navigation([trackingPage, routingPage, ticketingPage])
+        pg = st.navigation([trackingPage, routingPage, feedbackPage])
         with st.sidebar:
             st.write(f"Logged in as {st.session_state.user}")
+            logout_bt = st.button("Logout",icon=":material/logout:")
+            if logout_bt:
+                st.session_state.logged_in = False
+                st.rerun()
         pg.run()
 
 if __name__ == "__main__":
